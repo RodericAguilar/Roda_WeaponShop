108a4c2017c91747925007a27e3fbba882102f56
Tony Stark <76168122+tony-stark-17@users.noreply.github.com>
2022-05-20 10:39:25 +0530

    Update app.js

--- html/app.js
+++ html/app.js
@@ -4,11 +4,13 @@ window.addEventListener('load', function() {
 
 var weaponname = null 
 var weaponprice = null
+var shopid = null
 window.addEventListener('message', function(event) {
     var v = event.data
 
     switch(v.action) {
         case 'openShop':
+                shopid = v.shop
                 $('.contenedor').show(500)
                 $('.Appendxd').append(`
                     <div class="Texto" id="${v.name}">
@@ -71,7 +73,8 @@ $(function(){
         $.post('https://Roda_WeaponShop/BuyWeapon', JSON.stringify({
             arma : weaponname,
             price : weaponprice,
-            tipo : 'money'
+            tipo : 'money',
+            shop : shopid
         })); 
     })  
 
@@ -79,7 +82,8 @@ $(function(){
         $.post('https://Roda_WeaponShop/BuyWeapon', JSON.stringify({
             arma : weaponname,
             price : weaponprice,
-            tipo : 'bank'
+            tipo : 'bank',
+            shop : shopid
         })); 
     })
 })
